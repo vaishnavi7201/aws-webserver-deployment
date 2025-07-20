8388c404507f41592a749a24a6b36e166827f7f3
Vaishnavi U P <vaishnaviurupanja7@gmail.com>
2025-07-20 12:34:03 +0000

    Commits

--- script.sh
+++ script.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+aws configure
+
+# aws cli need to be installed before runnig this command and once done, config aws cli with this command and add access key IDs which are created during IAM user creation
+
 aws s3 cp /application s3://12static12bucket12 --recursive
+
+# copy files present in /application directory to the s3 bucket named 12static12bucket12.
+# To copy the entire directory to the bucket --recursive command is recommended
